25597486028157398c97b5bf15f75b61b927c104
Ivan <ivanvladimir0435@gmail.com>
2019-08-28 18:59:04 -0600

    Idk what I'm doin
    Aaaaaaaaaaaa

--- lua/entities/npc_vj_extinction_antlion/shared.lua
+++ lua/entities/npc_vj_extinction_antlion/shared.lua
@@ -5,7 +5,7 @@ ENT.Author 			= "DrVrej"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Purpose 		= "Spawn it and fight with it!"
 ENT.Instructions 	= "Click on the spawnicon to spawn it."
-ENT.Category		= ""
+ENT.Category		= "A"
 
 if (CLIENT) then
 	local Name = "Headcrab"
